dfb84ba856100c4faac42971e7daf78f1ae48001
Brandon <91856930+Branndon1@users.noreply.github.com>
2023-01-29 21:18:00 -0500

    Exercise03
    Exercise03

---
+++ Excise03/excise03.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.random.rand(10)
+mean = np.mean(arr)
+median = np.median(arr)
+stddev = np.std(arr)
+
+
+print("Random Numbers:", arr)
+print("Mean:", mean)
+print("Median:", median)
+print("Standard Deviation:", stddev)
